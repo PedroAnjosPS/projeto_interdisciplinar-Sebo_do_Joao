685c6821cc06d7a9918364ba1035490bd2ddc275
Pedro <plinhares832@gmail.com>
2026-09-01 09:51:22 -0300

    Adicionando as Entities

--- java/seboDoJoao/src/main/java/br/com/interdisciplinar/sebodojoao/model/Entrega.java
+++ java/seboDoJoao/src/main/java/br/com/interdisciplinar/sebodojoao/model/Entrega.java
@@ -1,10 +1,21 @@
 package br.com.interdisciplinar.sebodojoao.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "entregas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Entrega {
-    // Atributos
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     private String transportadora;
     private String linkRastreio;
     private String codigoRastreio;
@@ -12,98 +23,4 @@ public class Entrega {
     private LocalDateTime dataEntrega;
     private LocalDateTime dataPrevisaoEntrega;
     private StatusEntrega status;
-
-    // Construtores
-    public Entrega() {}
-
-    public Entrega(int id, String transportadora, String linkRastreio, String codigoRastreio, LocalDateTime dataPostagem, LocalDateTime dataEntrega, LocalDateTime dataPrevisaoEntrega, StatusEntrega status) {
-        this.id = id;
-        this.transportadora = transportadora;
-        this.linkRastreio = linkRastreio;
-        this.codigoRastreio = codigoRastreio;
-        this.dataPostagem = dataPostagem;
-        this.dataEntrega = dataEntrega;
-        this.dataPrevisaoEntrega = dataPrevisaoEntrega;
-        this.status = status;
-    }
-
-    // Métodos acessores e modificadores
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTransportadora() {
-        return transportadora;
-    }
-
-    public void setTransportadora(String transportadora) {
-        this.transportadora = transportadora;
-    }
-
-    public String getLinkRastreio() {
-        return linkRastreio;
-    }
-
-    public void setLinkRastreio(String linkRastreio) {
-        this.linkRastreio = linkRastreio;
-    }
-
-    public String getCodigoRastreio() {
-        return codigoRastreio;
-    }
-
-    public void setCodigoRastreio(String codigoRastreio) {
-        this.codigoRastreio = codigoRastreio;
-    }
-
-    public LocalDateTime getDataPostagem() {
-        return dataPostagem;
-    }
-
-    public void setDataPostagem(LocalDateTime dataPostagem) {
-        this.dataPostagem = dataPostagem;
-    }
-
-    public LocalDateTime getDataEntrega() {
-        return dataEntrega;
-    }
-
-    public void setDataEntrega(LocalDateTime dataEntrega) {
-        this.dataEntrega = dataEntrega;
-    }
-
-    public LocalDateTime getDataPrevisaoEntrega() {
-        return dataPrevisaoEntrega;
-    }
-
-    public void setDataPrevisaoEntrega(LocalDateTime dataPrevisaoEntrega) {
-        this.dataPrevisaoEntrega = dataPrevisaoEntrega;
-    }
-
-    public StatusEntrega getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusEntrega status) {
-        this.status = status;
-    }
-
-    // toString
-    @Override
-    public String toString() {
-        return "\n\t{" +
-                "\n\t\tId: " + id
-                + "\n\t\tTransportadora: " + transportadora
-                + "\n\t\tLink de rastreio: " + linkRastreio
-                + "\n\t\tCódigo de rastreio: " + codigoRastreio
-                + "\n\t\tData de postagem: " + dataPostagem
-                + "\n\t\tData de entrega: " + dataEntrega
-                + "\n\t\tData de previsão de entrega: " + dataPrevisaoEntrega
-                + "\n\t\tStatus: " + status
-                + "\n\t}";
-    }
 }
